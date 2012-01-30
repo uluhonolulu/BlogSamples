f485d6c64d94dbf31b31417118fff9e915f48a2b
Artem Smirnov <uluhonolulu@gmail.com>
2012-01-30 13:23:21 +0400

    Added controller, models, Dal

---
+++ SubclassingBinder/Web/Models.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web {
+	public abstract class ReferenceModel {
+		public int Id { get; set; }
+	}
+
+	public class ArticleModel : ReferenceModel {
+		public string ArticleName { get; set; }
+	}
+
+	public class WebPageModel : ReferenceModel {
+		public string Url { get; set; }
+	}
+
+	public static class DAL {
+		private static readonly IList<ReferenceModel> _data = new List<ReferenceModel>
+		                                                      {
+				new ArticleModel {ArticleName = "On the meaning of life", Id = 1},
+				new WebPageModel {Url = "http://www.meaningoflife.com", Id = 2}
+		    };
+
+		public static IEnumerable<ReferenceModel> GetData() {
+			return _data;
+		} 
+	}
+}
---
+++ SubclassingBinder/Web/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web {
+	public class SampleController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+
+		public ActionResult Edit(int id) {
+			return null;
+		}
+
+		[HttpPost]
+		public ActionResult UpdateReference() {
+			return null;
+		}
+	}
+}
