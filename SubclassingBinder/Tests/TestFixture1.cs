bf1e838eb41b898b35b235af69696712de5aa25f
Artem Smirnov <uluhonolulu@gmail.com>
2012-01-31 13:15:36 +0400

    Got the tests running

--- SubclassingBinder/Tests/TestFixture1.cs
+++ SubclassingBinder/Tests/TestFixture1.cs
@@ -1,6 +1,7 @@
 ﻿using Ivonna.Framework;
 using Ivonna.Framework.Mvc;
 using NUnit.Framework;
+using Web;
 
 namespace Tests {
 	[TestFixture, RunOnWeb]
@@ -8,6 +9,14 @@ namespace Tests {
 		[Test]
 		public void Test() {
 			new TestSession().Get("");
+		}
+		[Test]
+		public void TestModelSubclassing() {
+			var response = new TestSession().Post("/Sample/UpdateReference",
+				new {
+					ModelType = "MvcApplication1.CustomModelBinders.ModelSubclassing.ArticleModel"
+				});
+			Assert.IsInstanceOf<ArticleModel>(response.ActionMethodParameters["model"]);
 		}
 	}
 }
